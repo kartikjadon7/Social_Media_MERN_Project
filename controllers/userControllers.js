72b44408621d107fc4fef1ef8deda81a5e9c9b99
kartikjadon7 <kartikjadon7@gmail.com>
2026-04-15 22:23:00 +0530

    dev branch

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,8 +4,9 @@ const PostLike = require("../models/PostLike");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Follow = require("../models/Follow");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
+// ================= TOKEN HELPERS =================
 const getUserDict = (token, user) => {
   return {
     token,
@@ -22,6 +23,7 @@ const buildToken = (user) => {
   };
 };
 
+// ================= REGISTER =================
 const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
@@ -32,8 +34,6 @@ const register = async (req, res) => {
 
     const normalizedEmail = email.toLowerCase();
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
     const existingUser = await User.findOne({
       $or: [{ email: normalizedEmail }, { username }],
     });
@@ -42,13 +42,18 @@ const register = async (req, res) => {
       throw new Error("Email and username must be unique");
     }
 
+    // HASH PASSWORD ✅
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({
       username,
       email: normalizedEmail,
       password: hashedPassword,
     });
 
-    const token = jwt.sign(buildToken(user), process.env.TOKEN_KEY);
+    const token = jwt.sign(buildToken(user), process.env.TOKEN_KEY, {
+      expiresIn: "7d",
+    });
 
     return res.json(getUserDict(token, user));
   } catch (err) {
@@ -56,6 +61,7 @@ const register = async (req, res) => {
   }
 };
 
+// ================= LOGIN =================
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -72,13 +78,16 @@ const login = async (req, res) => {
       throw new Error("Email or password incorrect");
     }
 
+    // COMPARE PASSWORD ✅
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
       throw new Error("Email or password incorrect");
     }
 
-    const token = jwt.sign(buildToken(user), process.env.TOKEN_KEY);
+    const token = jwt.sign(buildToken(user), process.env.TOKEN_KEY, {
+      expiresIn: "7d",
+    });
 
     return res.json(getUserDict(token, user));
   } catch (err) {
@@ -87,12 +96,14 @@ const login = async (req, res) => {
   }
 };
 
+// ================= FOLLOW =================
 const follow = async (req, res) => {
   try {
     const { userId } = req.body;
     const followingId = req.params.id;
 
-    const existingFollow = await Follow.find({ userId, followingId });
+    // FIXED: use findOne
+    const existingFollow = await Follow.findOne({ userId, followingId });
 
     if (existingFollow) {
       throw new Error("Already following this user");
@@ -106,6 +117,29 @@ const follow = async (req, res) => {
   }
 };
 
+// ================= UNFOLLOW =================
+const unfollow = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const followingId = req.params.id;
+
+    // FIXED: use findOne
+    const existingFollow = await Follow.findOne({ userId, followingId });
+
+    if (!existingFollow) {
+      throw new Error("Not already following user");
+    }
+
+    // FIXED: deleteOne instead of remove()
+    await existingFollow.deleteOne();
+
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+// ================= UPDATE USER =================
 const updateUser = async (req, res) => {
   try {
     const { userId, biography } = req.body;
@@ -116,7 +150,7 @@ const updateUser = async (req, res) => {
       throw new Error("User does not exist");
     }
 
-    if (typeof biography == "string") {
+    if (typeof biography === "string") {
       user.biography = biography;
     }
 
@@ -128,25 +162,7 @@ const updateUser = async (req, res) => {
   }
 };
 
-const unfollow = async (req, res) => {
-  try {
-    const { userId } = req.body;
-    const followingId = req.params.id;
-
-    const existingFollow = await Follow.find({ userId, followingId });
-
-    if (!existingFollow) {
-      throw new Error("Not already following user");
-    }
-
-    await existingFollow.remove();
-
-    return res.status(200).json({ data: existingFollow });
-  } catch (err) {
-    return res.status(400).json({ error: err.message });
-  }
-};
-
+// ================= FOLLOWERS =================
 const getFollowers = async (req, res) => {
   try {
     const userId = req.params.id;
@@ -159,6 +175,7 @@ const getFollowers = async (req, res) => {
   }
 };
 
+// ================= FOLLOWING =================
 const getFollowing = async (req, res) => {
   try {
     const userId = req.params.id;
@@ -171,6 +188,7 @@ const getFollowing = async (req, res) => {
   }
 };
 
+// ================= GET USER =================
 const getUser = async (req, res) => {
   try {
     const username = req.params.username;
@@ -206,23 +224,24 @@ const getUser = async (req, res) => {
   }
 };
 
+// ================= RANDOM USERS =================
 const getRandomUsers = async (req, res) => {
   try {
     let { size } = req.query;
 
     const users = await User.find().select("-password");
 
-    const randomUsers = [];
+    size = parseInt(size) || 5;
 
     if (size > users.length) {
       size = users.length;
     }
 
+    const randomUsers = [];
     const randomIndices = getRandomIndices(size, users.length);
 
     for (let i = 0; i < randomIndices.length; i++) {
-      const randomUser = users[randomIndices[i]];
-      randomUsers.push(randomUser);
+      randomUsers.push(users[randomIndices[i]]);
     }
 
     return res.status(200).json(randomUsers);
@@ -232,14 +251,17 @@ const getRandomUsers = async (req, res) => {
   }
 };
 
+// ================= HELPER =================
 const getRandomIndices = (size, sourceSize) => {
   const randomIndices = [];
+
   while (randomIndices.length < size) {
     const randomNumber = Math.floor(Math.random() * sourceSize);
     if (!randomIndices.includes(randomNumber)) {
       randomIndices.push(randomNumber);
     }
   }
+
   return randomIndices;
 };
 
